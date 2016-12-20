0780ebcea794a513f4549aeecb13bb777699f650
fernote7 <fernote7@gmail.com>
2016-12-20 17:14:23 -0200

    DESISTO DESSE ASSIGN.

--- R/series_bacen_anna.R
+++ R/series_bacen_anna.R
@@ -13,25 +13,18 @@ series_bacen <- function(arg1, ...){
     datas = format(Sys.Date(), "%d/%m/%Y")
     inputs = as.character(list(arg1, ...))
     len = seq_along(inputs)
-    serie = mapply(paste0, "serie_", inputs, USE.NAMES = FALSE)
+    series = mapply(paste0, "serie_", inputs, USE.NAMES = FALSE)
 
-    for (i in len){ assign(serie[i],
+    for (i in len){ assign(series[i],
                              getURL(paste0('http://api.bcb.gov.br/dados/serie/bcdata.sgs.', inputs[i],
                                            '/dados?formato=csv&dataInicial=01/04/2003&dataFinal=',datas),
                                             ssl.verifyhost=FALSE, ssl.verifypeer=FALSE))}
 
 
-    for (i in len){
-
-        assign(serie[i], read.csv(textConnection(eval(as.symbol(
-        serie[i]))), header=T))
-
-        #assign(eval(as.symbol(serie[i]))$dados, gsub(' .*$','', eval(as.symbol(serie[i]))$data))
-
-    }
+    for (i in len){ assign(series[i], read.csv(textConnection(eval(as.symbol(
+        series[i]))), header=T))}
 
 
-
-    for(i in len) {write.csv(eval(as.symbol(serie[i])), file = paste0(serie[i], ".csv"))}
+    for(i in len) {write.csv(eval(as.symbol(series[i])), file = paste0(series[i], ".csv"))}
 
 }
