de1665f362f335a2bda3dc54885e33d22c7258e0
fernote7 <fernote7@gmail.com>
2016-12-20 15:26:25 -0200

    Adding packrat to the package.

--- R/series_bacen.R
+++ R/series_bacen.R
@@ -13,16 +13,18 @@ series_bacen <- function(arg1, ...){
     datas = format(Sys.Date(), "%d/%m/%Y")
     inputs = as.character(list(arg1, ...))
     len = seq_along(inputs)
+    series = mapply(paste0, "serie_", inputs, USE.NAMES = FALSE)
 
-    for (i in len){ assign(paste0("serie_", inputs[i]),
+    for (i in len){ assign(series[i],
                              getURL(paste0('http://api.bcb.gov.br/dados/serie/bcdata.sgs.', inputs[i],
                                            '/dados?formato=csv&dataInicial=01/04/2003&dataFinal=',datas),
                                             ssl.verifyhost=FALSE, ssl.verifypeer=FALSE))}
 
 
-    for (i in len){ assign(paste0("series_", inputs[i]), read.csv(textConnection(eval(as.symbol(
-        paste0("serie_", inputs[i])))), header=T))}
+    for (i in len){ assign(series[i], read.csv(textConnection(eval(as.symbol(
+        series[i]))), header=T))}
 
 
+    for(i in len) {write.csv(eval(as.symbol(series[i])), file = paste0(series[i], ".csv"))}
 
 }
