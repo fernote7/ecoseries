1f6ffd921577cc4f7b8e7ebdae0f094b14f71771
fernote7 <fernote7@gmail.com>
2016-12-20 15:00:15 -0200

    no message.

---
+++ R/exemplo.R
@@ -0,0 +1,43 @@
+#' A Cat Function
+#'
+#' This function allows you to express your love of cats.
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords cats
+#' @export
+#' @import RCurl
+#' @examples
+#' cat_function()
+
+#library(RCurl)
+#require(chron)
+
+datas = format(Sys.Date(), "%d/%m/%Y")
+
+
+#myfile2 <- getURL(paste0('http://api.bcb.gov.br/dados/serie/bcdata.sgs.24364/dados?formato=csv&dataInicial=01/04/2003&dataFinal=',datas), ssl.verifyhost=FALSE, ssl.verifypeer=FALSE)
+
+#mydat2 <- read.csv(textConnection(myfile2), header=T)
+#head(mydat2)
+
+#mydat2$data2 = gsub(' .*$','',mydat2$data)
+
+#require(xlsx)
+#setwd("C:/Users/fernando.teixeira/Dropbox/Luciana+Maclá")
+#write.xlsx(x = mydat2, "serie_24364.xlsx", row.names = FALSE)
+
+#Physically open an excel file
+#shell.exec("filename")
+
+
+#teste = read.xlsx("Índice de Atividade Econômica do Bacen (IBC-BR) - Com ajuste.xlsm", sheetIndex = 1)
+
+#write.xlsx(teste, "teste.xlsx")
+
+
+# dados_bacen <- function(cod1, ...){
+#
+#     inputs = list(cod1, ...)
+#
+#
+# }
+
--- R/series_bacen.R
+++ R/series_bacen.R
@@ -4,17 +4,25 @@
 #' @param love Do you love cats? Defaults to TRUE.
 #' @keywords cats
 #' @export
+#' @import RCurl
 #' @examples
 #' cat_function()
 
 series_bacen <- function(arg1, ...){
-    
+
+    datas = format(Sys.Date(), "%d/%m/%Y")
     inputs = as.character(list(arg1, ...))
     len = seq_along(inputs)
-    
-    for (i in 1:len){ assign(paste0("serie_", inputs[i]),
-                             getURL(paste0('http://api.bcb.gov.br/dados/serie/bcdata.sgs.', inputs[i], '/dados?formato=csv&dataInicial=01/04/2003&dataFinal=',data), ssl.verifyhost=FALSE, ssl.verifypeer=FALSE))}
-    
-    
-    
+
+    for (i in len){ assign(paste0("serie_", inputs[i]),
+                             getURL(paste0('http://api.bcb.gov.br/dados/serie/bcdata.sgs.', inputs[i],
+                                           '/dados?formato=csv&dataInicial=01/04/2003&dataFinal=',datas),
+                                            ssl.verifyhost=FALSE, ssl.verifypeer=FALSE))}
+
+
+    for (i in len){ assign(paste0("series_", inputs[i]), read.csv(textConnection(eval(as.symbol(
+        paste0("serie_", inputs[i])))), header=T))}
+
+
+
 }
