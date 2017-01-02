2cb9e80d2a97527746aa4a5c03b3fc930206fd05
fernote7 <fernote7@gmail.com>
2017-01-02 10:56:07 -0200

    Improving the sidra function documentation.

--- R/series_sidra.R
+++ R/series_sidra.R
@@ -1,9 +1,11 @@
 #' A function to extract Sidra series using their API
+#' 
+#' The different parameters define a table and its dimensions (periods, variables, territorial units and classification) to be consulted. The parameters that define the territorial units can be used more than once, allowing multiple distinct territorial searches with one request. The parameters that define the classes may vary from table to table, with a maximum of 6. Henceforth, a Sidra table ranges between 3 and 9 differente dimension (3 mandatory - periodos, var and terr and 6 optional).
 #' @param cod_tabela Sidra series number.
 #' @param periodos Specifies the desired periods. Defaults to 'all'.
 #' @param var Specifies the desired variables. Defaults to 'allxp'.
 #' @param terr Specifies the desired territorial levels. Defaults to 'n1/1' - Brazil.
-#' @param classe Specifies the table classifications. Defaults to "".
+#' @param classes Specifies the table classifications. Defaults to "".
 #' @param dec Specifies the desired decimals in table numbers.
 #' @param header Logical. Either TRUE or FALSE.
 #' @param save A string specifying if data should be saved in csv or xlsx format. 
@@ -16,14 +18,14 @@
 #' ibge=series_sidra(cod_tabela = 1612, formato = "a", header=TRUE)    
 
 series_sidra <- function(cod_tabela, periodos = "all", var = "allxp", terr = "n1/1", 
-                         classe = "", dec = 2, header = FALSE, save = "", formato = "n"){
+                         classes = "", dec = 2, header = FALSE, save = "", formato = "n"){
     
-    # cod_tabela = 1612; periodos = "all"; var = "allxp"; terr = "n1/1"; classe = ""; 
+    # cod_tabela = 1612; periodos = "all"; var = "allxp"; terr = "n1/1"; classes = ""; 
     # dec=2; header=TRUE; save = ""; formato = "n"
 
     cod_tabela = as.character(cod_tabela)
     terr = gsub(' ','%', terr)
-    if (classe != "") {classe = paste0(classe, "/")}
+    if (classes != "") {classes = paste0(classes, "/")}
     if (formato != "c" & formato != "n" & formato != "u" & formato != "a"){ 
         stop("formato argument must be 'c', 'n', 'u' or 'a' ")}
     if ( header == TRUE | header == T) { 
@@ -33,7 +35,7 @@ series_sidra <- function(cod_tabela, periodos = "all", var = "allxp", terr = "n1
     } else { stop("header assume only TRUE or FALSE values")}
     
     tabela=RCurl::getURL(paste0("http://api.sidra.ibge.gov.br/values/",
-                         "t/", cod_tabela, "/", terr, "/", classe, "p/", periodos, 
+                         "t/", cod_tabela, "/", terr, "/", classes, "p/", periodos, 
                          "/v/", var, "/f/", formato, "/h/", header),
              ssl.verifyhost=FALSE, ssl.verifypeer=FALSE)
     
