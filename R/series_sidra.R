33bd8086620cc0263c4099ba075b0565e991b018
fernote7 <fernote7@gmail.com>
2017-01-12 16:14:42 -0200

    Sidra function now accepts multiple series as arguments.

--- R/series_sidra.R
+++ R/series_sidra.R
@@ -1,11 +1,10 @@
 #' A function to extract Sidra series using their API
 #' 
 #' The different parameters define a table and its dimensions (periods, variables, territorial units and classification) to be consulted. The parameters that define the territorial units can be used more than once, allowing multiple distinct territorial searches with one request. The parameters that define the classes may vary from table to table, with a maximum of 6. Henceforth, a Sidra table ranges between 3 and 9 differente dimension (3 mandatory - periods, var and terr and 6 optional).
-#' @param cod_tabela Sidra series number.
+#' @param x Sidra series number.
 #' @param from A string specifying where the series shall start. Defaults to 1980.
 #' @param to A string specifying where the series shall end. Defaults to current year.
 #' @param territory Specifies the desired territorial levels.
-#' @param dec Specifies the desired decimals in table numbers.
 #' @param header Logical. Either TRUE or FALSE.
 #' @param save A string specifying if data should be saved in csv or xlsx format. 
 #' Defaults to not saving.
@@ -13,13 +12,14 @@
 #' @export
 #' @import RCurl rjson
 #' @examples
-#' series_sidra(cod_tabela = 1612; from = ""; to = ""; territory = "brazil")
+#' sidra=series_sidra(x = c(1612), from = "", to = "", territory = "brazil")
+#' sidra=series_sidra(x = c(1358, 1612, 1432, 111,1234, 4216, 1, 3287), 
+#' from = "", to = "", territory = "brazil")
+#' sidra=series_sidra(x = c(1612,4216), from = "", to = "", territory = "state")
 
-
-
-series_sidra <- function(cod_tabela, from = "", to = "", territory = c(n1 = "brazil", n2 = "region", n3 = "state"), dec = 2, header = TRUE, save = ""){
+series_sidra <- function(x, from = "", to = "", territory = c(n1 = "brazil", n2 = "region", n3 = "state"), header = TRUE, save = ""){
     
-    cod_tabela = as.character(cod_tabela)
+    x = as.character(x)
     
     
     if (from == ""){
@@ -43,47 +43,62 @@ series_sidra <- function(cod_tabela, from = "", to = "", territory = c(n1 = "bra
     } else if (header == FALSE | header == F) { 
         header = "n"
     } else { stop("header assume only TRUE or FALSE values")}
-
-
-    tabela=RCurl::getURL(paste0("http://api.sidra.ibge.gov.br/values/",
-                         "t/", cod_tabela, "/", territory, "/", "p/", 
-                         data_init, "-", data_end,  
-                         "/v/", "allxp", "/f/", "a", "/h/", header),
-             ssl.verifyhost=FALSE, ssl.verifypeer=FALSE)
     
     
-    if (strsplit(tabela, " ")[[1]][1] == "Par\uE2metro") {
+    inputs = as.character(x)
+    len = seq_along(inputs)
+    serie = mapply(paste0, "serie_", inputs, USE.NAMES = FALSE)
+
+    for (i in len){
+        tabela=RCurl::getURL(paste0("http://api.sidra.ibge.gov.br/values/",
+                             "t/", inputs[i], "/", territory, "/", "p/", 
+                             data_init, "-", data_end,  
+                             "/v/", "allxp", "/f/", "a", "/h/", header),
+                 ssl.verifyhost=FALSE, ssl.verifypeer=FALSE)
         
-        param = strsplit(tabela, " ")[[1]][3]
-        param = substr(param, 2, nchar(param)-1)
-        stop(sprintf("The parameter %s is misspecified", param))
         
-        
-    } else{
-        t1 = paste("tabela", cod_tabela, sep="_")
-        tabela = rjson::fromJSON(tabela)
-        tabela=data.frame(do.call("rbind", tabela))
-        if (header == "y"){
+        if (strsplit(tabela, " ")[[1]][1] == "Par\uE2metro") {
             
-            colnames(tabela) = unlist(tabela[1,])
-            tabela = tabela[-1,]
+            stop("The parameters 'from', 'to' or both are misspecified")
+            
+            
+        } else if (strsplit(tabela, " ")[[1]][1] == "Tabela" & 
+                      strsplit(tabela, " ")[[1]][3] == "Tabela"){
+            
+            param = strsplit(tabela, " ")[[1]][2]
+            param = substr(param, 1, nchar(param)-1)
+            warning(sprintf("The table %s does not contain public data", param))
+            
+        } else{
+            t1 = paste("tabela", x, sep="_")
+            tabela = rjson::fromJSON(tabela)
+            tabela=data.frame(do.call("rbind", tabela))
+            if (header == "y"){
+                
+                colnames(tabela) = unlist(tabela[1,])
+                tabela = tabela[-1,]
+            }
+            
+            #Transformando a coluna V em valor
+            
+            valor = NULL
+            
+            id = which(colnames(tabela)=="V" | colnames(tabela)=="Valor")
+                
+                #tabela2 = tabela
+                #tabela2[tabela2[,id] ==  "..",id] <- NA
+                #tabela2[,id] <- as.numeric(tabela2[,id])
+                tabela[,id] = suppressWarnings(ifelse(tabela[,id]!="..", as.numeric(tabela[,id]),NA))
         }
-        
-        #Transformando a coluna V em valor
-        
-        valor = NULL
-        
-        id = which(colnames(tabela)=="V" | colnames(tabela)=="Valor")
-            
-            #tabela2 = tabela
-            #tabela2[tabela2[,id] ==  "..",id] <- NA
-            #tabela2[,id] <- as.numeric(tabela2[,id])
-            tabela[,id] = suppressWarnings(ifelse(tabela[,id]!="..", as.numeric(tabela[,id]),NA))
+    
+        assign(serie[i],tabela)
+        rm(tabela)
     }
+    
+    lista = list()
+    ls_df = ls()[grepl('data.frame', sapply(ls(), function(x) class(get(x))))]
+    for ( obj in ls_df ) { lista[obj]=list(get(obj)) }
 
-    assign(t1,tabela)
-    rm(tabela)
-
-    return(invisible(eval(as.symbol(t1))))
+    return(invisible(lista))
     
 }
