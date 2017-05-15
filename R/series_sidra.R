76d3483b9afb63a16da9c2a279f176f68cf54924
Fernando Ormonde Teixeira <fernando.teixeira@fgv.br>
2017-05-15 17:39:23 -0300

    .

--- R/series_sidra.R
+++ R/series_sidra.R
@@ -26,7 +26,8 @@
 #' # sidra=series_sidra(x = c(1618), from = c("201703"), to = c("201703"), 
 #' # territory = "brazil",
 #' # variable = 109, sections=list(c(39427), c(39437,39441)), cl = c(49, 48))
-#' # trim - x = 1620; from = 199001; to = 201701;  territory = "brazil"; sections=list(c(90687)); cl =c(11255); variable = 583
+#' # trim - x = 1620; from = 199001; to = 201701;  territory = "brazil"; 
+#' # sections=list(c(90687)); cl =c(11255); variable = 583
 
 
 
